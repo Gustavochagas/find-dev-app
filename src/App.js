08b070d0e201c1498b9c391fbc8397472f56852c
unknown <gustavochagasb@gmail.com>
2020-02-25 10:47:07 -0300

    init errors

--- src/App.js
+++ src/App.js
@@ -11,36 +11,60 @@ import DevForm from "./components/DevForm";
 
 function App() {
   const [devs, setDevs] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [errorSend, setErrorSend] = useState(false);
 
   useEffect(() => {
     async function loadDevs() {
-      const response = await api.get("/devs");
-
-      setDevs(response.data);
+      try {
+        const response = await api.get("/devs");
+        setDevs(response.data);
+        setLoading(false);
+      } catch {
+        setError(true);
+        setLoading(false);
+      }
     }
 
     loadDevs();
   }, []);
 
   async function handleAddDev(data) {
-    const response = await api.post("/devs", data);
+    try {
+      const response = await api.post("/devs", data);
 
-    setDevs([...devs, response.data]);
+      setDevs([...devs, response.data]);
+    } catch {
+      setErrorSend(true);
+    }
   }
 
   return (
     <div id="app">
-      <aside>
-        <strong>Cadastrar</strong>
-        <DevForm onSubmit={handleAddDev} />
-      </aside>
-      <main>
-        <ul>
-          {devs.map(dev => (
-            <DevItem key={dev._id} dev={dev} />
-          ))}
-        </ul>
-      </main>
+      {loading ? (
+        <div className="loading">Loading</div>
+      ) : (
+        <div>
+          <aside>
+            <strong>Cadastrar</strong>
+            <DevForm onSubmit={handleAddDev} error={errorSend} />
+          </aside>
+          <main>
+            {error && !loading ? (
+              <div className="error">
+                <p>Error to connecting to server...</p>
+              </div>
+            ) : (
+              <ul>
+                {devs.map(dev => (
+                  <DevItem key={dev._id} dev={dev} />
+                ))}
+              </ul>
+            )}
+          </main>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/DevForm/index.js
+++ src/components/DevForm/index.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 
-function DevForm({ onSubmit }) {
+function DevForm({ onSubmit, error }) {
   const [github_username, setGithubUsername] = useState("");
   const [techs, setTechs] = useState("");
   const [latitude, setLatitude] = useState("");
   const [longitude, setLongitude] = useState("");
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
@@ -22,6 +23,7 @@ function DevForm({ onSubmit }) {
   }, []);
 
   async function handleSubmit(e) {
+    setLoading(true);
     e.preventDefault();
 
     await onSubmit({
@@ -30,63 +32,83 @@ function DevForm({ onSubmit }) {
       latitude,
       longitude
     });
-
+    setLoading(false);
     setGithubUsername("");
     setTechs("");
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="input-block">
-        <label htmlFor="github_username">Usuário do Github</label>
-        <input
-          name="github_username"
-          id="github_username"
-          value={github_username}
-          onChange={e => setGithubUsername(e.target.value)}
-          required
-        />
-      </div>
-
-      <div className="input-block">
-        <label htmlFor="techs">Tecnologias</label>
-        <input
-          name="techs"
-          id="techs"
-          value={techs}
-          onChange={e => setTechs(e.target.value)}
-          required
-        />
-      </div>
-
-      <div className="input-group">
+    <div>
+      <form onSubmit={handleSubmit}>
         <div className="input-block">
-          <label htmlFor="latitude">Latitude</label>
+          <label htmlFor="github_username">Usuário do Github</label>
           <input
-            type="number"
-            name="latitude"
-            id="latitude"
+            name="github_username"
+            id="github_username"
+            value={github_username}
+            onChange={e => setGithubUsername(e.target.value)}
             required
-            value={latitude}
-            onChange={e => setLatitude(e.target.value)}
           />
         </div>
 
         <div className="input-block">
-          <label htmlFor="longitude">Longitude</label>
+          <label htmlFor="techs">Tecnologias</label>
           <input
-            type="number"
-            name="longitude"
-            id="longitude"
+            name="techs"
+            id="techs"
+            value={techs}
+            onChange={e => setTechs(e.target.value)}
             required
-            value={longitude}
-            onChange={e => setLongitude(e.target.value)}
           />
         </div>
-      </div>
 
-      <button type="submit">Salvar</button>
-    </form>
+        <div className="input-group">
+          <div className="input-block">
+            <label htmlFor="latitude">Latitude</label>
+            <input
+              type="number"
+              name="latitude"
+              id="latitude"
+              required
+              value={latitude}
+              onChange={e => setLatitude(e.target.value)}
+            />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="longitude">Longitude</label>
+            <input
+              type="number"
+              name="longitude"
+              id="longitude"
+              required
+              value={longitude}
+              onChange={e => setLongitude(e.target.value)}
+            />
+          </div>
+        </div>
+
+        <button type="submit">
+          {loading ? (
+            <svg viewBox="0 0 64 64" className="svg-loading">
+              <circle cx="32" cy="32" r="30"></circle>
+            </svg>
+          ) : (
+            "Submit"
+          )}
+        </button>
+      </form>
+      {error ? (
+        <div
+          className="error"
+          style="color: red; text-align: center; margin: 10px 0;"
+        >
+          Error to connecting to server...
+        </div>
+      ) : (
+        <div></div>
+      )}
+    </div>
   );
 }
 
